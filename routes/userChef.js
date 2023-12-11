2340aef9526ada7f0b0057a722f88b5639045b75
hartmanjonathan <jonathan.raissa@outlook.fr>
2023-12-11 14:49:49 +0100

    route userchef et recipes

--- routes/userChef.js
+++ routes/userChef.js
@@ -1,13 +1,47 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const UserChef = require('../models/userChef');
+const UserProfil = require('../models/userProfil');
 
+router.post('/upgradeToChef', (req, res) => {
+  // Vérifie si l'utilisateur est connecté
+  if (!req.body.user) {
+    res.status(401).json({ result: false, error: 'Unauthorized' });
+    return;
+  }
 
+  // Recherche l'utilisateur existant
+  UserProfil.findById(req.user._id, (err, existingUser) => {
+    if (err) {
+      res.status(500).json({ result: false, error: 'Error finding existing user' });
+      return;
+    }
 
-/* GET users listing. */
-router.get('/', (req, res, ) => {
-    res.send('respond with a resource');
+    if (!existingUser) {
+      res.status(404).json({ result: false, error: 'User not found' });
+      return;
+    }
+
+    // Crée une instance de UserChef en utilisant les informations de l'utilisateur existant
+    const newUserChef = new UserChef({
+      spécialisation: req.body.spécialisation,
+      userCompliment: req.body.userCompliment,
+      experience: req.body.experience,
+      passion: req.body.passion,
+      services: req.body.services,
+      userProfil: existingUser._id, // Utilise l'ID de l'utilisateur existant
+    });
+
+    // Sauvegarde l'utilisateur chef dans la base de données
+    newUserChef.save((err, savedUserChef) => {
+      if (err) {
+        res.status(500).json({ result: false, error: 'Error saving user chef details' });
+        return;
+      }
+
+      res.status(200).json({ result: true, savedUserChef });
+    });
   });
+});
 
-//CREATION, SUPPRESSION, MISE A JOUR (recette, note, autre?)
-
-  module.exports = router;
+module.exports = router;
