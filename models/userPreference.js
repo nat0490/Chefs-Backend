68de4954cd6c9c99a5e27cd266cfc6f863e9dd59
Nat <nathalie_sa@live.fr>
2023-12-13 14:27:15 +0100

    supression route userProfil by id connexion

--- models/userPreference.js
+++ models/userPreference.js
@@ -3,9 +3,8 @@ const mongoose = require('mongoose');
 
 
 const userPreferenceSchema = mongoose.Schema({
-    userProfil: {type: mongoose.Schema.Types.ObjectId, ref: 'userProfil'},
+    //userProfil: {type: mongoose.Schema.Types.ObjectId, ref: 'userProfil'},
     typeCuisine: String,
-    
 })
 
 const UserPreference = mongoose.model('userPreference', userPreferenceSchema);
