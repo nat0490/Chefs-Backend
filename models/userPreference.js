7cbcb29b99539b6d301a9326177ac5eda8ba3136
Nat <nathalie_sa@live.fr>
2023-12-13 15:34:13 +0100

    conflit

--- models/userPreference.js
+++ models/userPreference.js
@@ -3,11 +3,7 @@ const mongoose = require('mongoose');
 
 
 const userPreferenceSchema = mongoose.Schema({
-<<<<<<< HEAD
-    // userProfil: {type: mongoose.Schema.Types.ObjectId, ref: 'userProfil'},
-=======
     //userProfil: {type: mongoose.Schema.Types.ObjectId, ref: 'userProfil'},
->>>>>>> e4d0e71ad43c3957659bf3a62e1aa1340507fd4e
     typeCuisine: String,
 })
 
