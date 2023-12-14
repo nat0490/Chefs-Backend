8976a907ec18074a397a3d390f7d482a7e06fbc1
Nat <nathalie_sa@live.fr>
2023-12-14 16:37:17 +0100

    création route find user avec idProfil

--- routes/userChef.js
+++ routes/userChef.js
@@ -30,10 +30,25 @@ router.post('/upgradeToChef/:userId', (req, res) => {
 });
 
 
-// Récupérer les informations d'un UserChef
+// Récupérer les informations d'un UserChef par Id
 router.get('/:userChefId', (req, res) => {
   UserChef.findOne({ _id: req.params.userChefId })
     .populate("userProfil")
+    .exec()
+    .then(data => {
+      if (data) {
+        res.json({ result: true, data });
+      } else {
+        res.json({ result: false, message: "UserChef profile not found" });
+      }
+    })
+    
+});
+
+// Récupérer les informations d'un UserChef par ProfilId
+router.get('/find/:profilId', (req, res) => {
+  UserChef.findOne({ userProfil: req.params.profilId })
+    .populate("recipes")
     .exec()
     .then(data => {
       if (data) {
