b42c8f79940ac78755d0c0b41e4fd92d60b48f71
HARTMAN <jonathan.raissa@outlook.fr>
2023-12-14 13:39:20 +0100

    bonne version

--- models/userPreference.js
+++ models/userPreference.js
@@ -3,11 +3,7 @@ const mongoose = require('mongoose');
 
   // userProfil: {type: mongoose.Schema.Types.ObjectId, ref: 'userProfil'},
 const userPreferenceSchema = mongoose.Schema({
-<<<<<<< HEAD
   
-=======
-    //userProfil: {type: mongoose.Schema.Types.ObjectId, ref: 'userProfil'},
->>>>>>> 351ed44d6f8864f08489c085c6a3e8034a826a60
     typeCuisine: String,
 })
 
