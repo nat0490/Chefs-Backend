1ad16318cdf5858feaa0571d1873fb7edee809f8
Nat <nathalie_sa@live.fr>
2023-12-16 11:24:50 +0100

    modif schema userChef: pouvoir mettre plusieurs recette

--- models/userChef.js
+++ models/userChef.js
@@ -7,7 +7,7 @@ const userChefSchema = mongoose.Schema({
     passion: String,
     services: String,
     userProfil: {type: mongoose.Schema.Types.ObjectId, ref: 'userProfil'},
-    recipes: {type: mongoose.Schema.Types.ObjectId, ref: 'recipes'},
+    recipes: [{type: mongoose.Schema.Types.ObjectId, ref: 'recipes'}],
 })
 
 const UserChef = mongoose.model('userChef', userChefSchema);
