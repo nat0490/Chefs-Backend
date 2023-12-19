b37776e895973319db3c8948aa4685442c5dd38b
Pweal98 <christofcarabin@outlook.fr>
2023-12-19 09:59:18 +0100

    backend reload

--- routes/recipes.js
+++ routes/recipes.js
@@ -125,8 +125,6 @@ router.get('/:recipeId', (req, res) => {
 });
 
 
-<<<<<<< HEAD
-
 
 
 
@@ -137,25 +135,9 @@ router.get('/', (req,res) => {
   .then(recipes => {
       res.json({ result: true, recipes }) // je veux afficher les recettes 
   })
-=======
-// Récupérer les informations d'une recette spécifique
-router.get('/displayRecipes', (req, res) => {
-  const recipeId = '657f241b4a4338fb4a3d4927';
-  Recipes.findOne({ _id: recipeId })
-    .then(recipe => {
-      console.log('Recipes:', recipe);  // Ajoutez cette ligne
-      if (recipe) {
-        res.json({ result: true, recipe });
-      } else {
-        res.json({ result: false, message: 'Recipe not found' });
-      }
-    })
-    .catch(error => {
-      console.error('Error retrieving recipes:', error);
-      res.status(500).json({ result: false, message: 'Internal server error' });
-    });
->>>>>>> christof
 });
+
+
 
 
  //METTRE A JOUR  l'image
